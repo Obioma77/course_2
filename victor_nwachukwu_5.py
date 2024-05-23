e831d0cd9bd4b79106a44d794dbc406e6128315c
Obioma77 <davicta2000@gmail.com>
2024-05-23 10:20:05 +0100

    assignment update

--- victor_nwachukwu_5.py
+++ victor_nwachukwu_5.py
@@ -1,2 +1,14 @@
 str = "The quick brown fox jumps over the lazy dog"
 print(str)
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.title())
+print(str[::-1])
+print(str[::-1].title())
+print(str.count('a'))
+print(str_lower.count('the'))
+print(str.replace('the', 'a'))
+
+
+
