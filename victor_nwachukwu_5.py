894ec814171d3e738377392c04271f0d4103d3be
Obioma77 <davicta2000@gmail.com>
2024-05-30 23:44:41 +0100

    first commit

--- victor_nwachukwu_5.py
+++ victor_nwachukwu_5.py
@@ -1,14 +1,13 @@
-str = "The quick brown fox jumps over the lazy dog"
-print(str)
-print(len(str))
-print(str.upper())
-print(str.lower())
-print(str.title())
-print(str[::-1])
-print(str[::-1].title())
-print(str.count('a'))
-print(str_lower.count('the'))
-print(str.replace('the', 'a'))
+brands = ["Hermes", "Gucci", "Chanel", "Gucci",
+           "Louis Vuitton", "Hermes", "Chanel","hugo boss", 
+           "Guess", "Gucci", "Louis Vuitton"]
 
-
-
+brand_dict = {}
+unique_brands = set(brands)
+for brand in unique_brands:
+       first_letter = brand[0].upper()
+       if first_letter not in brand_dict:
+            brand_dict[first_letter] = [brand] 
+else:
+            brand_dict[first_letter].append(brand)
+print(brand_dict)
